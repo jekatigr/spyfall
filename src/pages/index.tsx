199fd9772c8a49d114906d1d50f8f34e748c9b7c
Gromov Evgeny <evgeniy.gromov@shtormtech.ru>
2019-12-23 01:30:39 +0300

    pwa test 5

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react';
 import Head from 'next/head';
 import App from 'components/App';
-import prefixedAsset from 'utils/assetPrefix';
+// import prefixedAsset from 'utils/assetPrefix';
 
 const IndexPage: React.FunctionComponent = () => (
     <>
         <Head>
             <title>Spyfall Game</title>
-            <link rel="manifest" href={prefixedAsset('manifest.json')} />
+            <link rel="manifest" href="/spyfall/manifest.json" />
         </Head>
         <App />
     </>
