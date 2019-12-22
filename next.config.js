450044677bc62f5c0e6d8647fb26045b4612959b
Gromov Evgeny <evgeniy.gromov@shtormtech.ru>
2019-12-22 23:18:59 +0300

    pwa test 1

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 const path = require('path');
+const withOffline = require('next-offline');
 const withLess = require('@zeit/next-less');
 
-module.exports = withLess({
+const nextConfig = {
     webpack(config, options) {
         config.resolve = {
             ...config.resolve,
@@ -13,5 +14,25 @@ module.exports = withLess({
         };
         return config;
     },
-    assetPrefix: process.env.ASSET_PREFIX,
-});
+    assetPrefix: process.env.ASSET_PREFIX || '',
+    // workboxOpts: {
+    //     globDirectory: '.',
+    //     globPatterns: ['./(public|src)/**/*.*']
+    // }
+    workboxOpts: {
+        swDest: 'static/service-worker.js',
+    },
+    experimental: {
+        async rewrites() {
+            return [
+                {
+                    source: '/service-worker.js',
+                    destination: '/_next/static/service-worker.js',
+                },
+            ]
+        },
+    },
+};
+
+module.exports = withOffline(withLess(nextConfig));
+
