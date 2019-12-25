d79f08a86cd8cf51e54465fa7e1ddad16fcc453b
linkovatanya <you@tatyana@particle.one>
2019-12-25 18:53:49 +0300

    added combineReducers function

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -1,8 +1,19 @@
-import { combineReducers } from 'redux';
 import app from './app';
 import game from './game';
 import playersInfo from './playersInfo';
 import settings from './settings';
+
+function combineReducers(reducers) {
+    return (state, action) :any => {
+        const newState = {};
+        Object.keys(reducers).forEach((r) => {
+            let reducerState = state === undefined ? undefined : state[r];
+            reducerState = reducers[r](reducerState, action);
+            newState[r] = reducerState;
+        });
+        return newState;
+    };
+}
 
 export default combineReducers({
     app,
