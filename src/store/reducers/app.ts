b1f373efe3bb3c241cc671b4f7f6be077bd63fc8
maconidolma <linkova93@gmail.com>
2019-12-29 18:04:43 +0300

    oops

--- src/store/reducers/app.ts
+++ src/store/reducers/app.ts
@@ -11,8 +11,8 @@ export const APP_STATES = {
 };
 
 const initialState = {
-    // appState: APP_STATES.START_SCREEN,
-    appState: APP_STATES.GAME,
+    appState: APP_STATES.START_SCREEN,
+    // appState: APP_STATES.GAME,
 };
 
 export default (state = initialState, action): typeof initialState => {
